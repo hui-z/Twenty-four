fd35edd55cec4edf09982188c37b476db4b3f701
huangyanyun <huangyanyun@kezaihui.com>
2018-03-23 18:22:38 +0800

    feature: create game activity

---
+++ app/src/main/java/game/com/twenty_four/GameActivity.kt
@@ -0,0 +1,32 @@
+package game.com.twenty_four
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_game.*
+
+/**
+ * Created by yanyunhuang on 23/03/2018.
+ */
+class GameActivity : AppCompatActivity()  {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_game)
+
+        go.setOnClickListener {
+            GameActivity.explicitStart(baseContext)
+            this.finish()
+        }
+
+    }
+
+    companion object {
+
+        fun explicitStart(context: Context) {
+            context?.startActivity(Intent(context, GameActivity::class.java))
+        }
+    }
+}
